0322853ec73df499766b6ebf4b1cba1e093a38ed
kelvinng2017 <kelvinngshuhui@gmail.com>
2021-08-22 20:41:30 +0800

    test2

--- 12_1類別的定義與應用/12_1_2操作類別的屬性與方法/12_2.py
+++ 12_1類別的定義與應用/12_1_2操作類別的屬性與方法/12_2.py
@@ -1,16 +1,16 @@
-"""
-12_2.py擴充12_1.py，列出銀行名稱與服務宗旨
-"""
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-
+"""
+擴充ch12_1.py,列出銀行名稱與服務宗旨
+"""
 class Banks():
     '''定義銀行類別'''
-    bankname = 'Taipei Bank'  # 定義屬性
+    bankname = "Taipei Bank" #定義屬性
+    def motto(self): #定義方法
+        return "以客為尊"
 
-    def motto(self):  # 定義方法
-        return "以客爲尊"
+userbank = Banks() #定義物件userbank
+print userbank.bankname
+print userbank.motto()
 
-
-userbank = Banks()
-print("目前服務的銀行是 ", userbank.bankname)
-print("銀行服務的理念是 ", userbank.motto())
