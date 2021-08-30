52e56ac8812f5731a20023441b06216f8a507c33
kelvinng2017 <kelvinngshuhui@gmail.com>
2021-08-30 21:30:48 +0800

    renew

--- 12_3類別的繼承/12_3_2如何取得基地類別的私有屬性/12_11_1.py
+++ 12_3類別的繼承/12_3_2如何取得基地類別的私有屬性/12_11_1.py
@@ -1,3 +1,19 @@
 """
 12_11_1.py Derived classes and base classes have the same simple instructions each time
 """
+
+
+class Person():
+    def __init__(self, name):
+        self.name = name
+
+
+class LawerPerson(Person):
+    def __init__(self, name):
+        self.name = name + "lawyer"
+
+
+hung = Person("kelviinng")
+lawer = LawerPerson("kelvinng")
+print(hung.name)
+print(lawer.name)
---
+++ 12_3類別的繼承/12_3_2如何取得基地類別的私有屬性/12_12.py
@@ -0,0 +1,44 @@
+"""
+12_9.py:下列是私有方法應用完整程式碼實例。
+"""
+
+
+class Banks():
+    """定義銀行類別"""
+
+    def __init__(self, uname):  # 初始化方法
+        self.__name = uname  # 設定私有存款者名字(增加兩個底線)
+        self.__balance = 0  # 設定私有開戶金額是0
+        self.bankname = "Taipei Bank"  # 設定私有銀行名稱
+        self.__rate = 30  # 設定美金與臺幣換匯比例
+        self.__service_charge = 0.01  # 換匯的服務費
+
+    def save_money(self, money):  # 設定存款方法
+        self.__balance = self.__balance + money  # 執行存款
+        print("存款 ", money, " 完成")  # 列印存款完成
+
+    def withdraw_money(self, money):  # 設定提款方法
+        self.__balance = self.__balance - money  # 執行提款
+        print("提款 ", money, " 完成")  # 列印提款完成
+
+    def get_balance(self):  # 獲得存款餘額
+        print(self.__name.title(), " 目前餘額: ", self.__balance)
+
+    def usa_to_taiwan(self, usa_d):  # 美金兌換臺幣方法
+        self.result = self.__cal_rate(usa_d)
+        return self.result
+
+    def __cal_rate(self, usa_d):  # 計算換匯是私有方法
+        return int(usa_d * self.__rate * (1-self.__service_charge))
+
+
+class Shilin_Banks(Banks):
+    # 定義士林分行
+    def __init__(self, uname):
+        self.bankname = "Taipei Bank - Shilin Branch"  # Define branch name
+
+
+jamesbank = Banks("James")  # Define Bank class object
+print("James's bank = ", jamesbank.bankname)  # print bank name
+hungbank = Shilin_Banks('hung')  # define shilin_Banks class object
+print("James's bank = ", hungbank.bankname)  # print bank name
